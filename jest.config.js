8621c055934f565861f774fb579500b8e03ab45e
Lucas Azambuja <lucas.azambuja@foregon.com>
2020-09-03 00:17:39 -0300

    chore: jest configs

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,12 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '<rootDir>/src/main/**',
+    '!<rootDir>/src/main/**',
     '!<rootDir>/src/**/*-protocols.ts',
     '!**/protocols/**',
     '!**/test/**'
   ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
